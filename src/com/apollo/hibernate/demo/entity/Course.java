cd511691eea90efe0bb497c67bd73b74c55ca12e
Miłosz <milosz.gus@gmail.com>
2019-07-08 12:52:08 +0200

    created Course Class with many-to-one relationship with Instructor

---
+++ src/com/apollo/hibernate/demo/entity/Course.java
@@ -0,0 +1,74 @@
+package com.apollo.hibernate.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="course")
+public class Course {
+		//1. annotate  class as an entity and map db table
+		//2. define fields
+		//3. annotate fields with db columns names
+		//4. create constructor
+		//5. getters setters
+		//6. toString method
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@ManyToOne(cascade={CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinColumn(name="instructor_id")
+	private Instructor instructor;
+	
+	public Course() {}
+
+	public Course(String title) {
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + "]";
+	}
+	
+	
+	
+}
--- src/com/apollo/hibernate/demo/entity/Instructor.java
+++ src/com/apollo/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.apollo.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -38,6 +42,10 @@ public class Instructor {
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
+	
+	@OneToMany(mappedBy="instructor",cascade={CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	private List<Course> courses;
+	
 	
 	public Instructor() {
 	}
@@ -93,8 +101,21 @@ public class Instructor {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", intructorDetail=" + instructorDetail + "]";
 	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
 	
-	
-	
-	
+	//add convience method for bi-directional relationship
+	public void add(Course tempCourses) {
+		if(courses==null) {
+			courses=new ArrayList<>();
+		}
+		courses.add(tempCourses);
+		tempCourses.setInstructor(this);
+	}
 }
